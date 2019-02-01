cadd6d3628a063a61e23f37be5c3ca6d98b14139
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2019-02-01 13:38:15 -0800

    Add fetch request and display events from url

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
+import icsToJson from "./icsToJson";
+
+let url =
+  "https://calendar.google.com/calendar/ical/pfutdblf1gi8jmfsvroh76f6jg%40group.calendar.google.com/public/basic.ics";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(url)
+      .then(response => response.text())
+      .then(this.onLoad);
+  }
+
+  onLoad = data => {
+    console.log(data);
+    data = icsToJson(data);
+    console.log(data);
+
+    this.setState({
+      data: data
+    });
+  };
+
   render() {
-    return <div className="App" />;
+    const { data } = this.state;
+
+    return (
+      <div>
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          {data.map(event => (
+            <li>
+              <p>{event.startDate}</p>
+              <p>{event.endDate}</p>
+              <p>{event.description}</p>
+              <p>{event.location}</p>
+              <p>{event.summary}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
